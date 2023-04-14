532ea11e827bd8ed6be4f0afb9216ab89b589343
Carlos Vásquez Rodriguez <carlouvasquez134@gmail.com>
2023-04-14 16:51:15 -0500

    usuario y listar-usuario
    Se añadieron los componentes listar y listar-usuario

--- easytravel/src/app/app.module.ts
+++ easytravel/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HotelsComponent } from './component/hotels/hotels.component';
 import { HotelsListarComponent } from './component/hotels/hotels-listar/hotels-listar.component';
-import {MatTableModule} from '@angular/material/table'
+import {MatTableModule} from '@angular/material/table';
+import { UsuarioComponent } from './component/usuario/usuario.component';
+import { UsuarioListarComponent } from './component/usuario/usuario-listar/usuario-listar.component'
 @NgModule({
   declarations: [
     AppComponent,
     HotelsComponent,
-    HotelsListarComponent
+    HotelsListarComponent,
+    UsuarioComponent,
+    UsuarioListarComponent
   ],
   imports: [
     BrowserModule,
