e273e518e948769b7526879433b0d8ccd801fbc4
Axel Calle <94157375+Na333m@users.noreply.github.com>
2023-04-26 22:03:53 -0500

    update

--- easytravel/src/app/component/places/places-creaedita/places-creaedita.component.ts
+++ easytravel/src/app/component/places/places-creaedita/places-creaedita.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormGroupDirective, FormControl } from '@angular/forms';
 import { Place } from 'src/app/model/places';
 import * as moment from 'moment';
 import { PlacesService } from 'src/app/service/places.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-places-PlacesCreaeditaComponent',
   templateUrl: './places-creaedita.component.html',
@@ -13,15 +13,8 @@ export class PlacesCreaeditaComponent implements OnInit {
   form: FormGroup = new FormGroup({});
   places: Place = new Place();
   mensaje: string = '';
-  id: number = 0;
-  edicion: boolean = false;
 
   ngOnInit(): void {
-    this.route.params.subscribe((data: Params) => {
-      this.id = data['id'];
-      this.edicion = data['id'] != null;
-      this.init();
-    })
     this.form = new FormGroup({
       id: new FormControl(),
       namePlace: new FormControl(),
@@ -30,45 +23,26 @@ export class PlacesCreaeditaComponent implements OnInit {
     });
   }
 
-  constructor(private pS: PlacesService, private router:Router, private route: ActivatedRoute) {}
+  constructor(private pS: PlacesService, private router:Router) {}
 
 
   aceptar(): void {
     this.places.id=this.form.value['id'];
-    this.places.namePlace=this.form.value['Nombre'];
-    this.places.descriptionPlace=this.form.value['Descripción'];
-    this.places.country=this.form.value['País'];
+    this.places.namePlace=this.form.value['namePlace'];
+    this.places.descriptionPlace=this.form.value['descriptionPlace'];
+    this.places.country=this.form.value['country'];
 
 
-    if(this.form.value['id'].length>0 &&['Nombre'].length>0  && this.form.value['Descripcion'].length>0 && this.form.value['País']>0)
+    if(this.form.value['namePlace'].length>0  && this.form.value['descriptionPlace'].length>0 && this.form.value['country'].length>0)
     {
-      if (this.edicion) {
-        this.pS.update(this.places).subscribe((data) => {
-          this.pS.list().subscribe(data => {
-            this.pS.setList(data);
-          })
+      this.pS.insert(this.places).subscribe(data=>{
+        this.pS.list().subscribe(data =>{
+          this.pS.setList(data)
         })
-      } else {
-        this.pS.insert(this.places).subscribe((data)=> {
-          this.pS.list().subscribe(data => {
-            this.pS.setList(data);
-          })
-        })
-      }
+      })
       this.router.navigate(['places']);
     }
     else{this.mensaje="Ingresa los datos correctamente"}
-  }
-  init() {
-    if (this.edicion) {
-      this.pS.listId(this.id).subscribe(data => {
-        this.form = new FormGroup({
-          id: new FormControl(data.id),
-          namePlace: new FormControl(data.namePlace),
-          descriptonPlace: new FormControl(data.descriptionPlace),
-          country: new FormControl(data.country)
-        })
-      })
-    }
+
   }
 }
