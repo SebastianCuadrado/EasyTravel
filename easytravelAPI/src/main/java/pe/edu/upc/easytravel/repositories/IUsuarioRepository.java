597b97d51ba0338d7b476e92dc610dd7daf5c432
SebastianCuadrado <130190983+SebastianCuadrado@users.noreply.github.com>
2023-06-07 12:02:34 -0500

    Delete easytravelAPI directory

--- easytravelAPI/src/main/java/pe/edu/upc/easytravel/repositories/IUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.easytravel.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.easytravel.entities.Usuarios;
-
-@Repository
-public interface IUsuarioRepository extends JpaRepository<Usuarios, Integer> {
-}
